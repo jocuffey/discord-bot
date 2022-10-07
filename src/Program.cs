a9072ab69dd08672d6bf023e68a56b0d56ebc550
Joanne Cuffey <joannecuffey@gmail.com>
2022-10-07 15:10:46 +0200

    Store token as user secret

---
+++ src/Program.cs
@@ -0,0 +1,25 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.UserSecrets;
+
+var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+var _client = new DiscordSocketClient();
+
+await MainAsync();
+
+async Task MainAsync()
+{
+    _client = new DiscordSocketClient();
+
+    // Token stored as user secret.
+    var token = config["Discord:Token"];
+
+    await _client.LoginAsync(TokenType.Bot, token);
+    await _client.StartAsync();
+
+    // Block this task until the program is closed.
+    await Task.Delay(-1);
+}
+
